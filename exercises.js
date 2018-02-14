efb1894abd4156cd905a031c7721faf6b6d16a4d
Christopher Phithamma <Yuuomi@Christophers-MacBook-Pro.local>
2018-02-13 19:11:57 -1000

    complete forloops

--- exercises.js
+++ exercises.js
@@ -8,7 +8,9 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 .
 "Now serving 20."
 */
-
+for(var i = 1; i<21; i++){
+	console.log("Now Serving:" + i);
+}
 
 /* 2) Pop Charts
 Write a for-loop that will iterate through the topFive array below and console.log the following message:
@@ -20,6 +22,9 @@ This week's chart buster is: '24K Magic.'
 */
 
 var topFive = ["Closer", "Starboy", "I Feel It Coming", "Let Me Love You", "24K Magic"];
+for(var i = 0; i<topFive.length; i++){
+	console.log("This week's chart buster is:" + " " + topFive[i] + " ");
+}
 
 
 /* 3) Dead Presidents
@@ -41,14 +46,27 @@ This function will iterate through the person parameter and console.log the foll
 
 "President person was a great leader."
 */
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for(var i = 0; i<presidents.length; i++){
+  console.log("The value at "+ [i] + " is " + presidents[i]);
+}
+console.log(presidents.length);
 
-
+function leaders(person){
+  for(var i =0;i<leaders.length; i++){
+    console.log("Presidents " + person[i] + " was a great leader.");
+  }
+}
+leaders(presidents);
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
-
-
+var stringOfNumbers = "";
+for(var i = 10; i<21; i++){
+stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
 
@@ -63,7 +81,8 @@ Declare a variable named `oddSum` and assign it to the Number value 0.
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
-  
+
+
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
